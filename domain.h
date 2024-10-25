95c9648d5706e445edaf9ee04042e159fd8138ca
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-10-25 22:03:13 +0300

    add class Book

--- domain.h
+++ domain.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "lib.h"
+
+class Book {
+public:
+	string title;
+	string author;
+	string ISBN;
+
+	Book() = default;
+	Book(const string& title, const string& author, const string& ISBN) : title(title), author(author), ISBN(ISBN){
+
+	}
+	void display();
+};
+		
