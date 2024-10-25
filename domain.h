ccec48726c3daa747a6ce74b6379b43818efeb8e
ForgeDom <maxstill2006@gmail.com>
2024-10-25 23:03:07 +0300

    Added changes to main, domain and lib

--- domain.h
+++ domain.h
@@ -1,3 +1,6 @@
+#ifndef DOMAIN_H
+#define DOMAIN_H
+
 #include "lib.h"
 
 class Book {
@@ -7,10 +10,10 @@ public:
     string isbn;
 
     Book() = default;
-    Book(const string& title, const string& author, const string& isbn) : title(title), author(author), isbn(isbn) {
+    Book(const string& title, const string& author, const string& isbn)
+        : title(title), author(author), isbn(isbn) {}
 
-    }
-    void display() {
+    void display() const {
         cout << "Title: " << title << ", Author: " << author << ", ISBN: " << isbn << '\n';
     }
 };
@@ -24,9 +27,9 @@ public:
     Author(const string& n, const string& nat)
         : name(n), nationality(nat) {}
 
-    void display() {
+    void display() const {
         cout << "Name: " << name << ", Nationality: " << nationality << '\n';
-    };
+    }
 };
 
 class Reader {
@@ -39,9 +42,9 @@ public:
     Reader(const string& n, int id, int b)
         : name(n), readerId(id), borrowedBooks(b) {}
 
-    void display() {
+    void display() const {
         cout << "Name: " << name << ", ID: " << readerId << ", Borrowed books: " << borrowedBooks << '\n';
-    };
+    }
 };
 
 class Librarian {
@@ -53,7 +56,61 @@ public:
     Librarian(const string& n, int id)
         : name(n), librarianId(id) {}
 
-    void display() {
+    void display() const {
         cout << "Librarian Name: " << name << ", ID: " << librarianId << '\n';
-    };
+    }
 };
+
+class Loan {
+public:
+    int readerId;
+    string bookIsbn;
+    string dueDate;
+
+    Loan() = default;
+    Loan(int rId, const string& bIsbn, const string& dDate)
+        : readerId(rId), bookIsbn(bIsbn), dueDate(dDate) {}
+
+    void display() const {
+        cout << "Reader ID: " << readerId << ", Book ISBN: " << bookIsbn << ", Due Date: " << dueDate << '\n';
+    }
+};
+
+class Library {
+private:
+    vector<Book> books;
+    vector<Reader> readers;
+    vector<Loan> loans;
+
+public:
+    void addBook(const Book& book) {
+        books.push_back(book);
+    }
+
+    void addReader(const Reader& reader) {
+        readers.push_back(reader);
+    }
+
+    void addLoan(const Loan& loan) {
+        loans.push_back(loan);
+    }
+
+    void displayBooks() const {
+        for (const auto& book : books) {
+            book.display();
+        }
+    }
+
+    void displayReaders() const {
+        for (const auto& reader : readers) {
+            reader.display();
+        }
+    }
+
+    void displayLoans() const {
+        for (const auto& loan : loans) {
+            loan.display();
+        }
+    }
+};
+#endif DOMAIN_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include "domain.h"
+#include "lib.h"
 
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    Library library;
+
+    vector<Book> books = loadBooks("data/books.csv");
+    for (const auto& book : books) {
+        library.addBook(book);
+    }
+
+    vector<Reader> readers = loadReaders("data/readers.csv");
+    for (const auto& reader : readers) {
+        library.addReader(reader);
+    }
+
+    vector<Loan> loans = loadLoans("data/loans.csv");
+    for (const auto& loan : loans) {
+        library.addLoan(loan);
+    }
+
+    cout << "Books in library:\n";
+    library.displayBooks();
+
+    cout << "\nReaders in library:\n";
+    library.displayReaders();
+
+    cout << "\nLoans:\n";
+    library.displayLoans();
+
+    return 0;
 }
