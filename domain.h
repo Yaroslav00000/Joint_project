c502784811f4767fae87bba6489119af1ae4fbb2
ForgeDom <maxstill2006@gmail.com>
2024-10-25 22:11:36 +0300

    Added class Author

--- domain.h
+++ domain.h
@@ -1,1 +1,30 @@
-#pragma once
+#include "lib.h"
+
+class Book {
+public:
+	string title;
+	string author;
+	string isbn;
+
+	Book() = default;
+	Book(const string& title, const string& author, const string& isbn) : title(title), author(author), isbn(isbn) {
+
+	}
+	void display() {
+		cout << "Title: " << title << ", Author: " << author << ", ISBN: " << isbn << '\n';
+	}
+};
+
+class Author {
+public:
+	string name;
+	string nationality;
+
+	Author() = default;
+	Author(const string& n, const string& nat)
+		: name(n), nationality(nat) {}
+
+	void display() {
+		cout << "Name: " << name << ", Nationality: " << nationality << '\n';
+	};
+};
